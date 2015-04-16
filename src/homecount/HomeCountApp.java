73c7b9bde6494ca5a13916c74a7cb2b106bc97d7
Marius Zilenas <mzilenas@gmail.com>
2015-04-16 15:51:00 +0300

    Doing updates to database data.

--- src/homecount/HomeCountApp.java
+++ src/homecount/HomeCountApp.java
@@ -19,7 +19,6 @@ import javax.swing.ScrollPaneConstants;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import javax.sql.RowSetListener;
 import javax.sql.rowset.CachedRowSet;
@@ -28,6 +27,12 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 import javax.swing.JTable;
 import com.sun.rowset.CachedRowSetImpl;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.sql.rowset.spi.SyncProviderException;
+import javax.sql.rowset.spi.SyncResolver;
 
 public class HomeCountApp
 	implements RowSetListener
@@ -40,6 +45,9 @@ public class HomeCountApp
 	IncomeExpenseTableModel IETableModel = null;
 	//Income/expense table
 	JTable table = null;
+
+	//IERecordUI fields
+	JTextField amountTf, nameTf, ondateTf;
 
 	public void setFrame(JFrame frame)
 	{
@@ -92,15 +100,24 @@ public class HomeCountApp
 	public void connectDB()
 	{
 		try
-		{ 
+		{
 			Class.forName("org.h2.Driver");
 			setConnection(
-					DriverManager.getConnection("jdbc:h2:~/homecount", "sa", ""));
+					DriverManager.getConnection("jdbc:h2:~/homecount", "sa", "")); 
 		}
 		catch (ClassNotFoundException | SQLException e)
 		{
 			System.out.println("DB connection error occured." + e);
 		}
+	}
+
+	public void createIETable()
+	{
+		setIETableModel(
+			new IncomeExpenseTableModel(
+				getIncomeExpenseTableContents())); 
+		getIETableModel().addEventsHandlersToRowSet(this);
+		getTable().setModel(getIETableModel());
 	}
 
 	/**
@@ -161,15 +178,154 @@ public class HomeCountApp
 		buttonBox.add(button2);
 		panel.add(buttonBox, BorderLayout.EAST);
 
-		setIETableModel(
-			new IncomeExpenseTableModel(
-				getIncomeExpenseTableContents()));
-		getIETableModel().addEventsHandlersToRowSet(this);
-
 		setTable(new JTable());
-		getTable().setModel(getIETableModel());
+		createIETable();
 
 		panel.add(new JScrollPane(getTable()), BorderLayout.NORTH);
+
+		//Record pane
+		amountTf = new JTextField();
+		nameTf   = new JTextField();
+		ondateTf = new JTextField();
+
+		JPanel rp = new JPanel(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.anchor    = GridBagConstraints.LINE_START;
+		c.weightx   = 0.25;
+		c.weighty   = 0;
+		c.gridx     = 0;
+		c.gridy     = 0;
+		c.gridwidth = 1;
+		rp.add(new JLabel("Amount"), c);
+
+		c           = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.anchor    = GridBagConstraints.LINE_END;
+		c.weightx   = 0.75;
+		c.weighty   = 0;
+		c.gridx     = 1;
+		c.gridy     = 0;
+		c.gridwidth = 1;
+		rp.add(amountTf, c);
+
+		c           = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.anchor    = GridBagConstraints.LINE_START;
+		c.weightx   = 0.25;
+		c.weighty   = 0;
+		c.gridx     = 0;
+		c.gridy     = 1;
+		c.gridwidth = 1;
+		rp.add(new JLabel("Name"), c);
+
+		c           = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.anchor    = GridBagConstraints.LINE_END;
+		c.weightx   = 0.75;
+		c.weighty   = 0;
+		c.gridx     = 1;
+		c.gridy     = 1;
+		c.gridwidth = 1;
+		rp.add(nameTf, c);
+
+		c           = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.anchor    = GridBagConstraints.LINE_START;
+		c.weightx   = 0.25;
+		c.weighty   = 0;
+		c.gridx     = 0;
+		c.gridy     = 2;
+		c.gridwidth = 1;
+		rp.add(new JLabel("On date"), c);
+
+		c           = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.anchor    = GridBagConstraints.LINE_END;
+		c.weightx   = 0.75;
+		c.weighty   = 0;
+		c.gridx     = 1;
+		c.gridy     = 2;
+		c.gridwidth = 1;
+		rp.add(ondateTf, c);
+
+		Box rpBBx = new Box(BoxLayout.X_AXIS);
+		JButton insertRow = new JButton("Insert row");
+		JButton updateRow = new JButton("Update row");
+		JButton updateDb  = new JButton("Update database");
+		JButton discard   = new JButton("Discard");
+		JButton deleteRow = new JButton("Delete row");
+		rpBBx.add(insertRow);
+		rpBBx.add(updateRow);
+		rpBBx.add(updateDb);
+		rpBBx.add(discard);
+		rpBBx.add(deleteRow);
+
+		insertRow.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent evt)
+				    {
+						System.out.println("Inserting row");
+						IERecord ier = readTextFields();
+
+						getIETableModel().insertRow(ier.getAmount(), ier.getName(), ier.getOndate());
+					}
+				});
+		updateRow.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent evt)
+					{
+						System.out.println("Updating row"); 
+						int selectedRow = getTable().getSelectedRow();
+						if (-1 != selectedRow)
+						{
+							getIETableModel().updateRow(
+								selectedRow, readTextFields());
+							syncWithTable();
+						}
+					}
+				});
+		updateDb.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent evt)
+				    {
+						//updates database with data from JTable
+						syncWithTable();
+						System.out.println("Updating database"); 
+					}
+				});
+		discard.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent evt)
+				    {
+						System.out.println("Clearing fields");
+						clearTextFields();
+						createIETable();
+					}
+				});
+
+		deleteRow.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent evt)
+					{
+						System.out.println("Deleting row");
+					}
+				});
+
+
+		c           = new GridBagConstraints();
+		c.fill      = GridBagConstraints.HORIZONTAL;
+		c.gridy     = 3;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		rp.add(rpBBx, c);
+
+		panel.add(rp, BorderLayout.SOUTH); 
 
 		frame.setVisible(true);
 		frame.pack();
@@ -182,12 +338,13 @@ public class HomeCountApp
 		try
 		{ 
 			crs = new CachedRowSetImpl();
-			crs.setType(ResultSet.TYPE_SCROLL_INSENSITIVE);
-			crs.setConcurrency(ResultSet.CONCUR_UPDATABLE);
-			ResultSet rs = getConnection().createStatement().executeQuery(
-					"SELECT amount, name, ondate FROM income_expense"
-					);
-			crs.populate(rs);
+			crs.setType(CachedRowSet.TYPE_SCROLL_INSENSITIVE);
+			crs.setConcurrency(CachedRowSet.CONCUR_UPDATABLE);
+			crs.setCommand(
+					  "SELECT id, amount, name, ondate FROM income_expense"); 
+
+			crs.setKeyColumns(new int[]{1});
+			crs.execute(getConnection());
 		}
 		catch (SQLException e)
 		{
@@ -366,9 +523,24 @@ public class HomeCountApp
 			//Empty intentionaly
 		}
 
+		public void updateRow(int rowIndex, IERecord r)
+		{
+			try
+			{
+				getRowSet().absolute(rowIndex+1);
+				getRowSet().updateString("amount", r.getAmount());
+				getRowSet().updateString("name"  , r.getName()  );
+				getRowSet().updateString("ondate", r.getOndate());
+				getRowSet().updateRow();
+			}
+			catch (SQLException e)
+			{
+				System.out.println("Error updating row."+e);
+			}
+		}
+
 		public void setValueAt(Object aValue, int rowIndex, int columnIndex)
 		{
-			System.out.format("Setting value for row %d column %d", rowIndex, columnIndex);
 		}
 	}
 
@@ -396,5 +568,105 @@ public class HomeCountApp
 	public void rowSetChanged(RowSetEvent event)
 	{
 		// intentionally empty
+	}
+
+	public static class IERecord
+	{
+		String amount;
+		String name  ;
+		String ondate;
+
+		public IERecord()
+		{
+		}
+
+		public IERecord(String amount, String name, String ondate)
+		{
+			this.amount = amount;
+			this.name   = name  ;
+			this.ondate = ondate;
+		}
+
+		public String getAmount()
+		{
+			return amount;
+		}
+
+		public String getName()
+		{
+			return name;
+		}
+
+		public String getOndate()
+		{
+			return ondate;
+		}
+	}
+
+	//Fills text fields with default data
+	public void clearTextFields()
+	{
+		setTextFields(new IERecord());
+	}
+
+	public IERecord readTextFields()
+	{
+		return new IERecord(amountTf.getText(), nameTf.getText(), ondateTf.getText());
+	}
+
+	public void setTextFields(IERecord ieRecord)
+	{
+		amountTf.setText(ieRecord.getAmount());
+		nameTf.setText(ieRecord.getName());
+		ondateTf.setText(ieRecord.getOndate());
+	}
+
+	public void syncWithTable()
+	{
+		try
+		{
+			// Must provide connection. Otherwise "Unable to get connection" occurs.
+			getIETableModel().getRowSet().acceptChanges(getConnection());
+		} 
+		catch (SyncProviderException e)
+		{
+			System.out.println("Updating failed."+e);
+			SyncResolver resolver = e.getSyncResolver();
+
+			Object crsValue;  // value in the RowSet object
+			Object resolverValue;  // value in the SyncResolver object
+			Object resolvedValue;  // value to be persisted 
+			try
+			{
+				while(resolver.nextConflict())
+				{
+					if(resolver.getStatus() == SyncResolver.UPDATE_ROW_CONFLICT)
+					{
+						int row = resolver.getRow();
+						CachedRowSet crs = getIETableModel().getRowSet();
+						crs.absolute(row);
+
+						int colCount = crs.getMetaData().getColumnCount();
+						for(int j = 1; j <= colCount; j++) 
+						{
+							if (resolver.getConflictValue(j) != null) 
+							{
+								crsValue = crs.getObject(j);
+								resolverValue = resolver.getConflictValue(j);
+								System.out.format("Crsvalue:%s , resolver:%s", crsValue, resolverValue);
+								// compare crsValue and resolverValue to determine
+								// which should be the resolved value (the value to persist)
+								//resolvedValue = crsValue;
+								//resolver.setResolvedValue(j, resolvedValue);
+							}
+						}
+					}
+				}
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Exception occured in resolver."+e);
+			}
+		}
 	}
 }
